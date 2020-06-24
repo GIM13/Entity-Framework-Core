e1812b8c5cea9cb5472853080491e8f59198c647
Genadi <gim13@abv.bg>
2020-06-24 17:53:42 +0300

    01EntityFrameworkIntroduction from 1 to 4

---
+++ 01EntityFrameworkIntroduction/02DatabaseFirst/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+
+namespace _02DatabaseFirst
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            using var connection = new SqlConnection(@"Server=.\SQLEXPRESS;
+                                                       Database=MinionsDB;
+                                                       Integrated Security=true");
+
+            var command = new SqlCommand("  SELECT v.Name, COUNT(mv.VillainId) AS MinionsCount " +
+                                         "    FROM Villains AS v " +
+                                         "    JOIN MinionsVillains AS mv ON v.Id = mv.VillainId " +
+                                         "GROUP BY v.Id, v.Name " +
+                                         "  HAVING COUNT(mv.VillainId) > 3 " +
+                                         "ORDER BY COUNT(mv.VillainId)", connection);
+
+            connection.Open();
+
+            using var reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine($"{reader["Name"]} - {reader["MinionsCount"]}");
+            }
+        }
+    }
+}
---
+++ 01EntityFrameworkIntroduction/03MinionNames/StartUp.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+
+namespace _03MinionNames
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            using var connection = new SqlConnection(@"Server=.\SQLEXPRESS;
+                                                       Database=MinionsDB;
+                                                       Integrated Security=true;");
+
+            connection.Open();
+
+             var commandVillain = new SqlCommand($"SELECT Name as VillainName FROM Villains WHERE Id = @Id;", connection);
+
+            commandVillain.Parameters.AddWithValue("@Id", input);
+
+            var readerVillain = commandVillain.ExecuteScalar();
+
+            if (readerVillain == null)
+            {
+                Console.WriteLine($"No villain with ID {input} exists in the database.");
+                return;
+            }
+
+            Console.WriteLine($"Villain: {readerVillain}");
+
+            var commandMinion = new SqlCommand($"  SELECT ROW_NUMBER() OVER (ORDER BY m.Name) as RowNum, m.Name as MinionName, m.Age " +
+                                                "    FROM MinionsVillains AS mv " +
+                                                "    JOIN Minions as m ON mv.MinionId = m.Id " +
+                                                "   WHERE mv.VillainId = @Id " +
+                                                "ORDER BY MinionName;", connection);
+
+            commandMinion.Parameters.AddWithValue("@Id", input);
+
+            using var readerMinion = commandMinion.ExecuteReader();
+
+            if (!readerMinion.HasRows)
+            {
+                Console.WriteLine($"(no minions)");
+                return;
+            }
+
+            int count = 1;
+
+            while (readerMinion.Read())
+            {
+                Console.WriteLine($"{count++}. {readerMinion["MinionName"]} {readerMinion["Age"]}");
+            }
+        }
+    }
+}
