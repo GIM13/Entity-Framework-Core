a3876a011d7ad6952344cd08e00dc893ebe16993
Genadi <gim13@abv.bg>
2020-06-25 15:45:54 +0300

    01EntityFrameworkIntroduction for 5 and 6

---
+++ 01EntityFrameworkIntroduction/05ChangeTownNamesCasing/StartUp.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+
+namespace _05ChangeTownNamesCasing
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            string countryName = Console.ReadLine();
+
+            using var connection = new SqlConnection(@"Server=.\SQLEXPRESS;
+                                                       Database=MinionsDB;
+                                                       Integrated Security=true;");
+
+            connection.Open();
+
+            var command = new SqlCommand("UPDATE Towns " +
+                                         "   SET Name = UPPER(Name) " +
+                                         " WHERE CountryCode = (SELECT c.Id" +
+                                                                " FROM Countries AS c " +
+                                                                "WHERE c.Name = @countryName)", connection);
+            command.Parameters.AddWithValue("@countryName", countryName);
+
+            command.ExecuteNonQuery();
+
+            command = new SqlCommand("SELECT t.Name  " +
+                                     "  FROM Towns as t " +
+                                     "  JOIN Countries AS c ON c.Id = t.CountryCode " +
+                                     " WHERE c.Name = @countryName", connection);
+            command.Parameters.AddWithValue("@countryName", countryName);
+
+            using var reader = command.ExecuteReader();
+
+            var towns = new List<string>();
+
+            while (reader.Read())
+            {
+                towns.Add(reader["Name"].ToString());
+            }
+
+            if (towns.Count == 0)
+            {
+                Console.WriteLine("No town names were affected.");
+            }
+            else
+            {
+                Console.WriteLine($"{towns.Count} town names were affected.");
+
+                Console.WriteLine("[" + string.Join(", ", towns) + "]");
+            }
+        }
+    }
+}
